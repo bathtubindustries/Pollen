4ecf66f37d4e3e1582479e11b1c8de96f7a3b331
manocha <manocha@usc.edu>
2014-04-02 23:17:03 -0700

    initial commit from local git

---
+++ jesusgetshigh/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  jesusgetshigh
+//
+//  Created by garv on 4/2/14.
+//  Copyright bathtubindustries 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
